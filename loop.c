8ba50750695d5e81e3b0e30ae2b397189486533f
r202203399ks <r202203399ks@jindai.jp>
2023-12-05 18:47:57 +0900

    change 10

--- loop.c
+++ loop.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    for (int i = 1; i <= 5; ++i) {
-        printf("Iteration %d\n", i);
-    }
-    return 0;
+  for (int i = 1; i <= 10; ++i) {
+    printf("Iteration %d\n", i);
+  }
+  return 0;
 }
